14f2eccd3cc51af91a6c6e3522ed1bf4705f5f0c
Alexander Klimenko <alexander.klimenko@itomy.ch>
2019-07-12 18:13:06 +0300

    init commit

---
+++ dozens_advices/android/app/src/main/java/ventum/zephyr/dozens_advices/MainActivity.java
@@ -0,0 +1,13 @@
+package ventum.zephyr.dozens_advices;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
